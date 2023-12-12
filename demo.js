941265188dddc8321a2ff424447c3ee9e7ccb554
Mishra5136 <59865012+Mishra5136@users.noreply.github.com>
2023-12-12 23:09:03 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,21 @@
+// Import necessary modules
+const express = require('express');
+
+// Create an Express application
+const app = express();
+const port = 3000;
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+    res.send('Welcome to TalentSprint Node.js Demo!');
+});
+
+// Define a route for the about page
+// app.get('/about', (req, res) => {
+//     res.send('About TalentSprint Node.js Demo');
+// });
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
